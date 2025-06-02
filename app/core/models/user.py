aa41b7cddc1597618e83fd9ff97cfcf9b64ce323
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-02 10:36:24 +0300

    🗃️ Add User Orm model

---
+++ app/core/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import BigInteger, Boolean, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.models.base import BaseOrm
+from app.core.models.mixins import TimestampMixin
+
+
+class UserOrm(BaseOrm, TimestampMixin):
+    tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=True)
+    last_name: Mapped[str] = mapped_column(String(30), nullable=True)
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
