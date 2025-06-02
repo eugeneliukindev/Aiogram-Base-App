7bb63690a22cf39d3f6ae330172478225a01168b
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-02 10:11:58 +0300

    🗃️ Add Base Orm abstract model

---
+++ app/core/models/base.py
@@ -0,0 +1,30 @@
+from typing import final
+
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+from app.config import settings
+from app.utils.case_converter import camel_case_to_snake_case
+
+
+class BaseOrm(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+    @final
+    def __repr__(self) -> str:
+        return (
+            f"<{self.__class__.__name__}>"
+            f"({', '.join([f'{k}={v!r}' for k, v in self.__dict__.items() if k != '_sa_instance_state'])})"
+        )
+
+    __str__ = __repr__
