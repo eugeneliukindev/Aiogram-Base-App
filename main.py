e7748af514886e93f2c8829c2bec6c24747c4651
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-04 13:05:17 +0300

    🔊 Add logs in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 import redis.asyncio as aredis
 from aiogram import Bot, Dispatcher
@@ -12,9 +13,12 @@ from app.handlers.commands import router as commands_router
 from app.middlewares import SessionDepMiddleware
 from app.utils.logger import configure_logging
 
+log = logging.getLogger(__name__)
+
 
 async def on_shutdown(bot: Bot) -> None:
     await db_manager.dispose()
+    log.info("Shutdown complete")
 
 
 async def main(
@@ -22,6 +26,7 @@ async def main(
     redis_host: str = settings.redis.host,
     redis_port: int = settings.redis.port,
 ) -> None:
+    log.info("Starting Bot...")
     redis = aredis.Redis(host=redis_host, port=redis_port, decode_responses=True)
     bot = Bot(token=bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await bot.delete_webhook(drop_pending_updates=True)
