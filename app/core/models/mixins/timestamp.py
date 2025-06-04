a088e2cce9cd4349651a3b446d4a1c020a48fe6c
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-04 22:44:41 +0300

    ♻️ Refactor timestamp from server_default to default UTC

--- app/core/models/mixins/timestamp.py
+++ app/core/models/mixins/timestamp.py
@@ -1,9 +1,13 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class TimestampMixin:
-    created_at: Mapped[datetime] = mapped_column(DateTime(), server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime(), server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.now(UTC),
+        onupdate=datetime.now(UTC),
+    )
