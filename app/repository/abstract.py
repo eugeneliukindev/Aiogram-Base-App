b830cce84b96bd55ff357d53b6e59be03e71bf14
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-04 13:02:40 +0300

    🏷️ Added and fixed type hints

--- app/repository/abstract.py
+++ app/repository/abstract.py
@@ -4,36 +4,35 @@ from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from collections.abc import Sequence
-
     from pydantic import BaseModel
+    from sqlalchemy import Result
     from sqlalchemy.ext.asyncio import AsyncSession
 
     from app.core.models import BaseOrm
 
 
-class AbstractRepository[M: BaseOrm, CreateS: BaseModel, UpdateS: BaseModel](ABC):
+class AbstractRepository[ModelT: BaseOrm, CreateST: BaseModel, UpdateST: BaseModel](ABC):
     @classmethod
     @abstractmethod
-    async def create(cls, session: AsyncSession, schema: CreateS) -> M | None | bool:
+    async def create(cls, session: AsyncSession, create_schema: CreateST) -> ModelT | None | bool:
         raise NotImplementedError()
 
     @classmethod
     @abstractmethod
-    async def get_by_id(cls, session: AsyncSession, id_: int) -> M | None:
+    async def get_by_id(cls, session: AsyncSession, id_: int) -> ModelT | None | bool:
         raise NotImplementedError()
 
     @classmethod
     @abstractmethod
-    async def get_all(cls, session: AsyncSession) -> Sequence[M]:
+    async def get_all(cls, session: AsyncSession) -> Result[tuple[ModelT, ...]] | bool:
         raise NotImplementedError()
 
     @classmethod
     @abstractmethod
-    async def update_by_id(cls, session: AsyncSession, id_: int, schema: UpdateS) -> M | None | bool:
+    async def update_by_id(cls, session: AsyncSession, id_: int, update_schema: UpdateST) -> ModelT | None | bool:
         raise NotImplementedError()
 
     @classmethod
     @abstractmethod
-    async def delete_by_id(cls, session: AsyncSession, id_: int) -> M | None | bool:
+    async def delete_by_id(cls, session: AsyncSession, id_: int) -> ModelT | None | bool:
         raise NotImplementedError()
