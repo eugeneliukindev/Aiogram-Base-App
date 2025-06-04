67c3af4676838abf7f67254bbfafddd35c5697b7
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-05 00:15:15 +0300

    🗃️ Refactor init migration with UTC timestamp

--- migrations/versions/2025_06_05_0005-416be42c7674_init_migration.py
+++ migrations/versions/2025_06_05_0005-416be42c7674_init_migration.py
@@ -1,8 +1,8 @@
 """init migration
 
-Revision ID: 605ed077609e
+Revision ID: 416be42c7674
 Revises:
-Create Date: 2025-06-04 00:14:19.059664
+Create Date: 2025-06-05 00:05:52.709014
 
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "605ed077609e"
+revision: str = "416be42c7674"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -28,8 +28,8 @@ def upgrade() -> None:
         sa.Column("last_name", sa.String(length=30), nullable=True),
         sa.Column("username", sa.String(length=50), nullable=True),
         sa.Column("is_active", sa.Boolean(), nullable=False),
-        sa.Column("created_at", sa.DateTime(), server_default=sa.text("now()"), nullable=False),
-        sa.Column("updated_at", sa.DateTime(), server_default=sa.text("now()"), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("updated_at", sa.DateTime(), nullable=False),
         sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
         sa.UniqueConstraint("tg_id", name=op.f("uq_users_tg_id")),
         sa.UniqueConstraint("username", name=op.f("uq_users_username")),
