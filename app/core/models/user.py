a54bf5ab03d815f55511ac1a746543aedd7afe1a
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-05 12:44:24 +0300

    ♻️ Change nullable=true to annotation ... | None

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -8,7 +8,7 @@ from app.core.models.mixins import TimestampMixin
 class UserOrm(BaseOrm, TimestampMixin):
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True)
     first_name: Mapped[str] = mapped_column(String(30))
-    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=True)
-    last_name: Mapped[str] = mapped_column(String(30), nullable=True)
+    username: Mapped[str | None] = mapped_column(String(50), unique=True)
+    last_name: Mapped[str | None] = mapped_column(String(30))
 
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
