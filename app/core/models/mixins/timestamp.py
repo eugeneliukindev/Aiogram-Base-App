863d0c376d8ba9ea5a70ddaa75ecdbc1c427b6be
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-05 10:11:41 +0300

    🗃️ Add datetime timezone

--- app/core/models/mixins/timestamp.py
+++ app/core/models/mixins/timestamp.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class TimestampMixin:
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime,
-        default=func.now(),
+        DateTime(timezone=True),
+        server_default=func.now(),
         onupdate=func.now(),
     )
