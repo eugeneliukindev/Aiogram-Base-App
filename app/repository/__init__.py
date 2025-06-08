00636dee6b5c4943c4d1a53f4e406b65f99b93e6
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-06-08 13:33:48 +0300

    🎨 Update package imports

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -1,5 +1,9 @@
 from .abstract import AbstractRepository
+from .base import BaseRepository
+from .user import UserRepository
 
 __all__ = [
     "AbstractRepository",
+    "BaseRepository",
+    "UserRepository",
 ]
